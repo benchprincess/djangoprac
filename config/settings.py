f4277cecbed78753a6e7ace7ab82100c89796d0d
jiwon hwang <realjiwon98@naver.com>
2025-01-19 16:51:11 +0900

    add mypy

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-_1w$r5@&ecmwc5bl)ifdjzo@w60llcir!w&+blw^hx&sj%3spx
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
---
+++ temp.py
@@ -0,0 +1,8 @@
+my_list: list[str | int] = [1, "1"]
+
+my_dict: dict[str, int] = {"a": 1}
+
+my_tuple: tuple[int, ...] = (
+    1,
+    3,
+)
