98701082584d41b0c1c972ed34a978c5b0c96fc1
jiwon hwang <realjiwon98@naver.com>
2025-01-23 17:46:18 +0900

    connect database

--- config/settings.py
+++ config/settings.py
@@ -78,7 +78,7 @@ pymysql.install_as_MySQLdb()
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.mysql",
-        "NAME":  "djangoprac",
+        "NAME": "djangoprac",
         "USER": "root",
         "PASSWORD": "900326",
         "HOST": "localhost",
